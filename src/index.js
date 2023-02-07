08a4e4a6d1a73e6fac5d05883e5b49b6eccf3553
Psycho4375 <119705159+Psycho4375@users.noreply.github.com>
2023-02-07 20:48:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function check(str, bracketsConfig) {
+    let stack = [];
+    let brackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+       for (let i = 0; i < str.length; i++){
+        const current = s[i];
+
+        if (isClosedBracket(current)){
+            if (brackets[current] !== stack.pop()) return false;
+        } else {
+            stack.push(current);
+        }
+    }
+
+    return stack.length === 0;
+}
+
+function isClosedBracket (ch) {
+    return [')','}',']'].indexOf(ch) > -1;
+}
 }
