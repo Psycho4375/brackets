c7b69b24303d96c83e555acd6ea75110ecfb7f55
Psycho4375 <119705159+Psycho4375@users.noreply.github.com>
2023-02-10 15:19:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  let current = '';
     for (i = 0; i < str.length; i++){
-            switch (bracketsConfig.length){
-                case 1:
-                    if (str[i] == bracketsConfig[0][0]){
-                    current + str.length[i];
-                    } else if (str[i] == bracketsConfig[0][1]){
-                        if (current.includes(bracketsConfig[0][0])){
-                            current--;
-                        }
-                    }
-                    break;
-                case 2:
-                    if (str[i] == bracketsConfig[0][0] || str[i] == bracketsConfig[1][0]){
-                    current + str.length[i];
-                    } else if (str[i] == bracketsConfig[0][1] || str[i] == bracketsConfig[1][1]){
-                        if (current.includes(bracketsConfig[0][0]) || current.includes(bracketsConfig[1][0])){
-                            current--;
-                        }
-                    }
-                    break;
-                case 3:
-                    if (str[i] == bracketsConfig[0][0] || str[i] == bracketsConfig[1][0] || str[i] == bracketsConfig[2][0]){
-                    current + str.length[i];
-                    } else if (str[i] == bracketsConfig[0][1] || str[i] == bracketsConfig[1][1] || str[i] == bracketsConfig[2][1]){
-                        if (current.includes(bracketsConfig[0][0]) || current.includes(bracketsConfig[1][0]) || current.includes(bracketsConfig[2][0])){
-                            current--;
-                        }
-                    }
-                    break;
-            }     
+        for (j = 0; j < bracketsConfig.length; j++){
+            if (bracketsConfig[j][1] === str[i] && bracketsConfig[j][0] === str[i-1]){
+                str = str.slice(0, i -1) + str.slice(i + 1, str.length);
+                i = 0;
+            }
+        }
     }
-    if (current == 0){
-        return true;
-    } else {
-        return false
-    }
+    return str.length === 0 ? true : false;
 }
