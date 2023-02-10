01a465570ff18e968a18cadbd25494cce587149d
Psycho4375 <119705159+Psycho4375@users.noreply.github.com>
2023-02-10 12:31:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-    let current = 0;
+    let current = '';
     for (i = 0; i < str.length; i++){
             switch (bracketsConfig.length){
                 case 1:
                     if (str[i] == bracketsConfig[0][0]){
-                    current++;
+                    current + str.length[i];
                     } else if (str[i] == bracketsConfig[0][1]){
-                        
-                    current--;
+                        if (current.includes(bracketsConfig[0][0])){
+                            current--;
+                        }
                     }
                     break;
                 case 2:
                     if (str[i] == bracketsConfig[0][0] || str[i] == bracketsConfig[1][0]){
-                    current++;
+                    current + str.length[i];
                     } else if (str[i] == bracketsConfig[0][1] || str[i] == bracketsConfig[1][1]){
-                    current--;
+                        if (current.includes(bracketsConfig[0][0]) || current.includes(bracketsConfig[1][0])){
+                            current--;
+                        }
                     }
                     break;
                 case 3:
                     if (str[i] == bracketsConfig[0][0] || str[i] == bracketsConfig[1][0] || str[i] == bracketsConfig[2][0]){
-                    current++;
+                    current + str.length[i];
                     } else if (str[i] == bracketsConfig[0][1] || str[i] == bracketsConfig[1][1] || str[i] == bracketsConfig[2][1]){
-                    current--;
+                        if (current.includes(bracketsConfig[0][0]) || current.includes(bracketsConfig[1][0]) || current.includes(bracketsConfig[2][0])){
+                            current--;
+                        }
                     }
                     break;
             }
@@ -33,5 +38,3 @@ module.exports = function check(str, bracketsConfig) {
     } else {
         return false
     }
-
-}
